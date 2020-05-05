4d212f2241ca5311f00a6254a4bd29ef8f447b16
kiranrajufixstream <kiran.raju@fixstream.com>
2020-05-05 14:13:17 +0530

    Initial commit

---
+++ src/Components/Perform.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Perform extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Perfrom Component</h1>
+            </div>
+        )
+    }
+}
